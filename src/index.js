f0274674755da009547095620bf5ab5db826af26
Satur <t.droppa@cinq.com.br>
2019-07-12 17:33:11 -0300

    Removes redux devtoll plugin from config

--- src/index.js
+++ src/index.js
@@ -7,25 +7,26 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App'
 import reducers from './redux/reducers'
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-let store;
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+//  let store;
+ let store = createStore(reducers, applyMiddleware(thunk));
 
-if (window.navigator.userAgent.includes('Chrome')) {
-    store = createStore(
-      reducers,
-      compose(
-        applyMiddleware(thunk),
-        composeEnhancer
-      )
-    );
-  } else {
-    store = createStore(
-      reducers,
-      compose(
-        applyMiddleware(thunk)
-      )
-    );
-  }
+// if (window.navigator.userAgent.includes('Chrome')) {
+//     store = createStore(
+//       reducers,
+//       compose(
+//         applyMiddleware(thunk),
+//         composeEnhancer
+//       )
+//     );
+//   } else {
+//     store = createStore(
+//       reducers,
+//       compose(
+//         applyMiddleware(thunk)
+//       )
+//     );
+//   }
 
 ReactDOM.render(
     <Provider store={store}>
